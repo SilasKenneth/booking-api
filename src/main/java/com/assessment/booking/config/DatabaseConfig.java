857968d98f0ebf2b2ab2bc57ce3fb1bf977700aa
Silas Kenneth <silaskenneth1@gmail.com>
2020-01-30 17:55:06 +0300

    Add custom config for the database

--- src/main/java/com/assessment/booking/config/DatabaseConfig.java
+++ src/main/java/com/assessment/booking/config/DatabaseConfig.java
@@ -16,7 +16,16 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource(){
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(dbUrl);
+        String[] parts = dbUrl.split("@");
+        String[] parts2 = parts[0].split("//");
+        String[] usernamePassword = parts2[1].split(":");
+        String username = usernamePassword[0];
+        String password = usernamePassword[1];
+        String realUrl = "jdbc:postgresql://"+parts[1];
+        hikariConfig.setJdbcUrl(realUrl);
+//        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
         return new HikariDataSource(hikariConfig);
     }
 }
---
+++ src/main/java/com/assessment/booking/controllers/Fake.java
@@ -0,0 +1,17 @@
+package com.assessment.booking.controllers;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Fake {
+    public static void main(String[] args) {
+        String url = "postgres://username:password@localhost:5432/d4kqugvqm4ccqs";
+        String[] parts = url.split("@");
+        String[] parts2 = parts[0].split("//");
+        String[] usernamePassword = parts2[1].split(":");
+        String username = usernamePassword[0];
+        String password = usernamePassword[1];
+        String real = "jdbc:"+parts2[0]+"//"+parts[1];
+        System.out.println(real);
+    }
+}
